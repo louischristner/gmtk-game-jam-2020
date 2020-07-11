883ac8fb82e5a71b965b9873787ddfdb18820867
louischristner <l.christner@orange.fr>
2020-07-11 16:59:52 +0200

    Create Tilemaps & PlayerMovement

---
+++ gmtk-game-jam-2020/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public float jumpForce = 20f;
+
+    public KeyCode keyUp = KeyCode.Z;
+    public KeyCode keyLeft = KeyCode.Q;
+    public KeyCode keyRight = KeyCode.D;
+
+    void FixedUpdate()
+    {
+        Vector2 movement = new Vector2(0, 0);
+
+        movement = MoveHorizontal(movement);
+        transform.Translate(
+            new Vector3(movement.x * moveSpeed * Time.fixedDeltaTime, 0, 0)
+        );
+    }
+
+    Vector2 MoveHorizontal(Vector2 movement)
+    {
+        if (Input.GetKey(keyLeft)) {
+            movement.x -= 1;
+        }
+
+        if (Input.GetKey(keyRight)) {
+            movement.x += 1;
+        }
+
+        return movement;
+    }
+}
