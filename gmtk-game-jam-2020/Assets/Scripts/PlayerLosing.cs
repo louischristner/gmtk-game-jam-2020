9e60fdbead3eeab4d491d633134f5bfd7181f01d
louischristner <l.christner@orange.fr>
2020-07-12 00:36:54 +0200

    Create spike tilemap & losing condition

---
+++ gmtk-game-jam-2020/Assets/Scripts/PlayerLosing.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerLosing : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject.tag == "spike") {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+}
